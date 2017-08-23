916de87904f33e092daa1bc97b7ebaf0e014b088
e200 <eleandro@inbox.ru>
2017-08-23 01:26:03 +0100

    Added toogle function for Umburguer button

---
+++ assets/js/master.js
@@ -0,0 +1,16 @@
+window.onload = function()
+{
+    $menuBtn = document.getElementsByClassName("umburg").item(0);
+
+    $menuBtn.onclick = function(b)
+    {
+        $class="umburg-active";
+
+        if(!$menuBtn.classList.contains($class)) {
+            $menuBtn.classList.add($class)
+        }
+        else {
+            $menuBtn.classList.remove($class)
+        }
+    }
+};
