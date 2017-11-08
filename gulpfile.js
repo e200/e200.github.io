1fd122271102ceb16467556d41d4fe09786fe31d
e200 <eleandro@inbox.ru>
2017-11-08 22:16:35 +0100

    Added gulp-clean-css, browser-sync, gulp-sourcemaps, gulp-pug, gulp-html-beautify, gulp-plumber.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,79 @@
 const
     gulp = require('gulp'),
+    
+    // Requires gulp-pumbler
+    pumbler = require('gulp-plumber'),
 
-    // Requires the gulp-sass plugin
+    // Requires gulp-sass
     sass = require('gulp-sass'),
+
+    // Requires gulp-clean-css
+    cleanCSS = require('gulp-clean-css'),
 
     // Requires gulp-autoprefixer
     autoprefixer = require('gulp-autoprefixer'),
-
-    browserSync = require('browser-sync');
-
-gulp.task('default', ['serve'], function(){
     
-});
+    // Requires browser-sync
+    browserSync = require('browser-sync'),
 
+    // Requires gulp-sourcemaps
+    sourceMaps = require('gulp-sourcemaps'),
+    
+    // Requires gulp-pug
+    pug = require('gulp-pug'),
+    
+    // Requires gulp-html-beautify
+    htmlBeautify = require('gulp-html-beautify');
+
+gulp.task('default', ['serve']);
+
+/**
+ * Serve task, starts watchers and browser.
+ */
 gulp.task('serve', function(){
     browserSync.init({
         server: './'
     });
 
-    gulp.watch('./assets/sass/*.sass', ['sass'])
+    gulp.watch('./assets/sass/*.sass', ['css'])
         .on('change', browserSync.reload);
-    gulp.watch('./index.html')
+    gulp.watch('./views/**', ['pug']);
+    gulp.watch('./index.html', ['beautifyHTML'])
         .on('change', browserSync.reload);
 })
 
-gulp.task('sass', function() {
+/**
+ * CSS task,compile SASS to CSS.
+ */
+gulp.task('css', function() {
     return gulp.src('./assets/sass/master.sass')
         .pipe(sass().on('error', sass.logError)) // Converts SASS to CSS eith gulp-sass
         .pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
-		})) // Adds vendor prefixes to CSS output.
+        })) // Adds vendor prefixes to CSS output
+        .pipe(sourceMaps.init()) // Maps the minified CSS
+        .pipe(cleanCSS({compatibility: 'ie8'})) // Minify the CSS
+        .pipe(sourceMaps.write()) // Write the minified CSS source map
         .pipe(gulp.dest('./assets/css'));
+});
+
+/**
+ * Pug task, compile PUG to HTML.
+ */
+gulp.task('pug', function(){
+    return gulp.src('./views/index.pug')
+    .pipe(pumbler()) // Errors handler
+    .pipe(pug()) // Compiles pug to HTML
+    .pipe(gulp.dest('./'));
+});
+
+/**
+ * Beautify HTML task, beautifies the index.html.
+ */
+gulp.task('beautifyHTML', function(){
+    return gulp.src('./index.html')
+    .pipe(pumbler()) // Errors handler
+    .pipe(htmlBeautify()) // Beautifies the html output
+    .pipe(gulp.dest('./'));
 });
