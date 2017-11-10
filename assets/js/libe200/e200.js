5a234f8498b8a88307ac6d5ffa0f362beabfe1a1
e200 <eleandro@inbox.ru>
2017-11-10 22:21:16 +0100

    Updated

--- assets/js/libe200/e200.js
+++ assets/js/libe200/e200.js
@@ -1,43 +1,76 @@
-window.e200 = (function(window){
+window.e200 = (function(window) {
+
+    function init() {
+        /**
+         * Adds a listener to the current el.
+         * 
+         * @param string   event_name 
+         * @param function callback 
+         */
+        function listener(event_name, callback) {
+            el.addEventListener(event_name, callback, false)
+        }
+
+        function toggleClass(class_name) {
+            if (el.classList.contains(class_name))
+                el.classList.remove(class_name)
+            else
+                el.classList.add(class_name)
+        }
     
-    var el = null
+        function recursiveResolve(node) {
+            console.log(node)
+            if (typeof node != 'undefined') {
+                node.on = listener
+                node.toggleClass = toggleClass
+    
+                var l = node.childElementCount
+
+                if (l) {
+                    childrens = node.children
+    
+                    for (var i = 0; i < l; i++) {
+                        recursiveResolve(childrens[i])
+                    }
+                }
+            }
+        }
+        
+        recursiveResolve(document.body)
+
+        //console.log(document.querySelectorAll('.line'))
+    }
+
+    window.onload = init    
 
     var self = function e200(selector) {
+        // Current el
+        var el
+
+        /**
+         * Sets the main function that will
+         * be loaded on window.onload
+         */
+        if (typeof selector == 'function') {
+            //document.onload = selector
+
+            return
+        }
         
-        if (typeof selector == 'function')
-            window.onload = selector
+        if (typeof selector == 'object')
+            el = selector
+
+        /**
+         * Quering the selector.
+         * Preparing it to be used
+         * with this library.
+         */
         else if (typeof selector == 'string') {
-            el = window.document.querySelectorAll(selector)
-
-            initElement()
-
-            return el
+            el = document.querySelectorAll(selector)
         }
+
+        return el
     }
-
-    function initElement() {
-        var l = el.length;
-        
-        if (l == 1) {
-            el = el.item(0)
-
-            el.on = listener
-        } else if (l > 1) {
-            el.forEach(function(elms) {
-                elms.on = listener
-            });
-        }
-    }
-
-    function listener(event_name, callback)
-    {
-        console.log(el)
-        el.addEventListener(event_name, callback)
-    }
-
-    //self.alert = function(){
-        //console.log(this)
-    //}
 
     return self
 })(window)
--- assets/js/master.js
+++ assets/js/master.js
@@ -1,74 +1,37 @@
 e200(function(){
-    e200('.umburg').on('click', function(e){
-        e.preventDefault()
-    })
-    e200('.line').on('click', function(){
-        alert()
-    })
+    //e200('span').on('click', function(e){
+    //    e.preventDefault()
+    //})
+
+    // // Sets a delay for start the transition.
+    // setTimeout(function(){
+    //     var words = e200('.words')
+
+    //     // Transition cycle.
+    //     setInterval(function(){
+            
+    //         // Getting the current displayed thing i love.
+    //         currentThing = words.find('.active');
+
+    //         // Getting the next thing i love to be displayed.
+    //         nextThing = currentThing.nextSibling;
+
+    //         /**
+    //          * If nextThing is null, that means we're
+    //          * in the last thing i like.
+    //          * 
+    //          * So, we should restart the cycle from top
+    //          * to the bottom again.
+    //          * 
+    //          * Otherwise we just keep displaying one by other.
+    //          */
+    //         if (nextThing === null) {
+    //             toggleClass(thing_i_love.firstChild, active_classname);
+    //             toggleClass(thing_i_love.lastChild, active_classname);
+    //         } else {
+    //             toggleClass(currentThing, active_classname);
+    //             toggleClass(nextThing, active_classname);
+    //         }
+    //     }, 3000);
+    // }, 3000);
 })
-
-// window.onload = function()
-// {
-//     $menuBtn = document.getElementsByClassName("umburg").item(0);
-
-//     $menuBtn.onclick = function(b)
-//     {
-//         b.preventDefault();
-        
-//         $class="umburg-active";
-
-//         if(!$menuBtn.classList.contains($class)) {
-//             $menuBtn.classList.add($class)
-//         }
-//         else {
-//             $menuBtn.classList.remove($class)
-//         }
-//     }
-
-//     // Useful on minify.
-//     var active_classname = 'active';
-
-//     thing_i_love = document.querySelector('.things_i_love');
-
-//     /**
-//      * Toggle a class name in an element.
-//      * 
-//      * @param object el 
-//      * @param string classname 
-//      */
-//     function toggleClass(el, classname)
-//     {
-//         if (el.classList.contains(classname))
-//             el.classList.remove(classname)
-//         else
-//             el.classList.add(classname)
-//     }
-
-//     // Sets a delay for the transition start.
-//     setTimeout(function(){
-//         // Transition cycle.
-//         setInterval(function(){
-//             // Getting the current displayed thing i love.
-//             currentThing = thing_i_love.querySelector('.active');
-//             // Getting the next thing i love to be displayed.
-//             nextThing = currentThing.nextSibling;
-
-//             /**
-//              * If nextThing is null, that means we're
-//              * in the last thing i like.
-//              * 
-//              * So, we should restart the cycle from top
-//              * to the bottom again.
-//              * 
-//              * Otherwise we just keep displaying one by other.
-//              */
-//             if (nextThing === null) {
-//                 toggleClass(thing_i_love.firstChild, active_classname);
-//                 toggleClass(thing_i_love.lastChild, active_classname);
-//             } else {
-//                 toggleClass(currentThing, active_classname);
-//                 toggleClass(nextThing, active_classname);
-//             }
-//         }, 3000);
-//     }, 3000);
-// };
