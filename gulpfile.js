ebb0a8db4269461c5fcd07b3cf695afcb2b2d97c
e200 <eleandro@inbox.ru>
2017-11-11 22:18:12 +0100

    Required uglify, concat, insert. Moved all paths to their specifics path consts. Rewroted all tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,14 @@ const
     // Requires gulp-pumbler
     pumbler = require('gulp-plumber'),
 
+    // Requires gulp-uglify
+    uglify = require('gulp-uglify'),
+
     // Requires gulp-sass
     sass = require('gulp-sass'),
 
-    // Requires gulp-clean-css
-    cleanCSS = require('gulp-clean-css'),
+    // Requires gulp-concat
+    concat = require('gulp-concat'),
 
     // Requires gulp-autoprefixer
     autoprefixer = require('gulp-autoprefixer'),
@@ -22,71 +25,102 @@ const
     // Requires gulp-pug
     pug = require('gulp-pug'),
     
-    // Requires gulp-html-beautify
-    htmlBeautify = require('gulp-html-beautify'),
-    
     // Requires gulp-insert
-    insert = require('gulp-insert');
+    insert = require('gulp-insert')
 
-gulp.task('default', ['serve']);
+const
+    projectFolder = './',
+    viewsFolder = projectFolder +'views/',
+    assetsFolder = projectFolder + 'assets/',
+    
+    jsFolder = assetsFolder + 'js/',
+    cssFolder = assetsFolder + 'css/',
+    sassFolder = assetsFolder + 'sass/'
+
+gulp.task('default', ['all'])
 
 /**
- * Serve task, starts watchers and browser.
+ * JS task, minifies and concats all JS
+ * files into a single one.
  */
-gulp.task('serve', function(){
-    browserSync.init({
-        server: './'
-    });
+gulp.task('js', function() {
+    const outputFile = 'e200.js'
+    const uglifyOptions = {
+        compress: {
+            drop_debugger: true,
+            toplevel: true
+        },
+        mangle: {
+            toplevel: true
+        }
+    }
 
-    gulp.watch('./assets/sass/*.sass', ['css'])
-        .on('change', browserSync.reload);
-    gulp.watch('./views/**', ['pug']);
-    gulp.watch('./index.html', ['beautifyHTML+header'])
-        .on('change', browserSync.reload);
-    gulp.watch('./assets/js/**')
-        .on('change', browserSync.reload);
+    return gulp.src([
+            jsFolder + 'libe200/element_functions.js',
+            jsFolder + 'libe200/set_functions_recursively.js',
+            jsFolder + 'libe200/bootstrapper.js',
+            jsFolder + 'libe200/e200.js',
+            jsFolder + 'e200.github.io.js'
+        ])
+        .pipe(pumbler())
+        .pipe(concat(outputFile)) // Concats all JS files
+        .pipe(uglify(uglifyOptions)) // Minifies the output
+        .pipe(gulp.dest(jsFolder))
 })
 
 /**
- * CSS task,compile SASS to CSS.
+ * Compiles my root SASS file
  */
-gulp.task('css', function() {
-    return gulp.src('./assets/sass/master.sass')
-        .pipe(sass().on('error', sass.logError)) // Converts SASS to CSS eith gulp-sass
-        .pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-        })) // Adds vendor prefixes to CSS output
-        .pipe(sourceMaps.init()) // Maps the minified CSS
-        .pipe(cleanCSS({compatibility: 'ie8'})) // Minify the CSS
-        .pipe(sourceMaps.write()) // Write the minified CSS source map
-        .pipe(gulp.dest('./assets/css'));
-});
+gulp.task('sass', function (){
+    var sassOptions = {
+        outputStyle: 'compressed'
+    }
+    var autoprefixerOptions = {
+        browsers: ['last 2 versions'],
+        cascade: false
+    }
+
+    return gulp.src(sassFolder + 'e200.sass')
+        .pipe(sass(sassOptions)
+            .on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(cssFolder))
+})
 
 /**
- * Pug task, compile PUG to HTML.
+ * Compiles my PUG files to HTML
+ * 
+ * Also prepends my header to the output
  */
 gulp.task('pug', function(){
-    var header = '<!--\n' +
+    const header = '<!--\n\n' +
+    '       ___   ___   ___\n' +
     '   ___|___\\ / _ \\ / _ \\\n' + 
     ' / _ \\ __) | | | | | | |\n' +
     '|  __// __/| |_| | |_| |\n' +
-    ' \\___|_____|\\___/ \\___/\n' + 
-    '-->';
-    
-    return gulp.src('./views/index.pug')
-    .pipe(pumbler()) // Errors handler
-    .pipe(pug()) // Compiles pug to HTML
-    .pipe(insert.prepend(header)) // Prepends my header to the HTML output
-    .pipe(gulp.dest('./'));
-});
+    ' \\___|_____|\\___/ \\___/\n\n' + 
+    '-->\n\n'
+
+    return gulp.src(viewsFolder + '**.pug')
+        .pipe(pumbler())
+        .pipe(pug({pretty: true}))
+        .pipe(insert.prepend(header))
+        .pipe(gulp.dest(projectFolder))
+})
+
+gulp.task('all', ['js', 'sass', 'pug'])
 
 /**
- * Beautify HTML task, beautifies the index.html.
+ * SASS watcher, watches any
+ * changes in SASS files and
+ * refresh browser when they
+ * happen.
  */
-gulp.task('beautifyHTML+header', function(){
-    return gulp.src('./index.html')
-    .pipe(pumbler()) // Errors handler
-    .pipe(htmlBeautify()) // Beautifies the html output
-    .pipe(gulp.dest('./'));
-});
+gulp.task('sass:watch', function(){
+    browserSync.init({
+        server: projectFolder
+    })
+
+    gulp.watch(sassFolder + '**', ['sass'])
+        .on('change', browserSync.reload)
+})
