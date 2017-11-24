ae92c03367417ec572e4e77d91b88b5ea22fe968
e200 <eleandro@inbox.ru>
2017-11-24 17:23:42 +0100

    Now i'm just toggling the header.

--- assets/js/e200.js
+++ assets/js/e200.js
@@ -1,12 +1,8 @@
 $(function(){
     $('.humburguer').on('click', function(e){
        e.preventDefault();
-       
-       $(this).toggleClass('active');
-       
-        for (var i = 0; i < 5; i++) {
-            $('.nav-anim-el-' + i).toggleClass('active');
-        }       
+              
+        $('.header').toggleClass('active');    
     })
 
     // Sets a delay for start the transition.
