0c2cf7acfce00887434c7a8286e00f5817554516
e200 <eleandro@inbox.ru>
2018-01-06 15:47:46 +0100

    Added "Download CV" button

--- assets/js/e200.js
+++ assets/js/e200.js
@@ -27,10 +27,10 @@ $(function(){
         var me = $(this);
 
         me.html('<div class="spinner">\
-                            <div class="bounce1"></div>\
-                            <div class="bounce2"></div>\
-                            <div class="bounce3"></div>\
-                      </div>');
+                    <div class="bounce1"></div>\
+                    <div class="bounce2"></div>\
+                    <div class="bounce3"></div>\
+                </div>');
 
         setTimeout(function(){
             me.html('Successfully!');
