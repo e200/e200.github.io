d025245f76914d9ab0ec573c8a97e4be19663961
e200 <eleandro@inbox.ru>
2018-01-20 20:47:01 +0100

    Added _path

---
+++ gulp/_path.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dist: {
+        css: './assets/dist/css/',
+        js:  './assets/dist/js/',
+    },
+        src: {
+        sass:  './assets/src/sass/',
+        js:    './assets/src/js/',
+        views: './assets/src/views/'
+    }
+}
