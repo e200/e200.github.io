62e07512db093bf47026819a666221189de23dc0
e200 <eleandro@inbox.ru>
2018-01-22 19:52:23 +0100

    Added filename from path

--- gulp/dev.js
+++ gulp/dev.js
@@ -1,19 +1,23 @@
 const
-    gulp         = require('gulp')
-    plumber      = require('gulp-plumber')
-    rename       = require('gulp-rename')
-    sass         = require('gulp-sass')
-    pug          = require('gulp-pug')
-    data         = require('gulp-data')
-    fs           = require('fs')
-    path         = require('path')
-    path_        = require('./path_')
+    gulp    = require('gulp')
+    plumber = require('gulp-plumber')
+    rename  = require('gulp-rename')
+    sass    = require('gulp-sass')
+    pug     = require('gulp-pug')
+    data    = require('gulp-data')
+    fs      = require('fs')
+    path    = require('path')
+    path_   = require('./path_')
 
 gulp.task('pug', function(){
     return gulp.src(path_.src.views + 'index.pug')
         .pipe(plumber())
         .pipe(data(function(file){
-            return JSON.parse(fs.readFileSync(path_.src.views + 'data/' + path.basename(file.path).slice(0, -4) + '.json'));
+            let filename = path.basename(file.path).slice(0, -4)
+
+            return JSON.parse(
+                fs.readFileSync(path_.src.views + 'data/' + filename + '.json')
+            );
         }))
         .pipe(pug({pretty: true}))
         .pipe(gulp.dest('./'))
