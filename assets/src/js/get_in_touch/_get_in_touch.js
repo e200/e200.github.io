051a6f18fd73c8b09c76c07d0219516271c0e4c8
e200 <eleandro@inbox.ru>
2018-02-22 20:47:33 +0100

    Added  smile()

--- assets/src/js/get_in_touch/_get_in_touch.js
+++ assets/src/js/get_in_touch/_get_in_touch.js
@@ -1,32 +1,84 @@
-var spinnerHTML = '<div class="spinner">\
-                    <div class="bounce1"></div>\
-                    <div class="bounce2"></div>\
-                    <div class="bounce3"></div>\
-                   </div>';
+var submitButton = $('#get-in-touch__form__submit-button'),
+    smileIcon    = $('.get-in-touch__smile');
 
+const spinnerHTML = '<div class="spinner">\
+                        <div class="bounce1"></div>\
+                        <div class="bounce2"></div>\
+                        <div class="bounce3"></div>\
+                    </div>';
 
+function smile(status = true) {
 
-$('#get-in-touch__form__submit-button').on('click', function() {
-  
-  var me = $(this);
+    if (status === false) {
+        
+        smileIcon
+            .addClass('fa-frown-o')
+            .removeClass('fa-meh-o');
 
-  $('.get-in-touch__smile')
-      .removeClass('fa-smile-o')
-      .addClass('fa-meh-o');
+    } else if (status === null) {
 
-  me.html();
+        smileIcon
+            .removeClass('fa-smile-o')
+            .addClass('fa-meh-o');
 
-  setTimeout(function(){
-      $('.get-in-touch__smile')
-          .addClass('fa-smile-o')
-          .removeClass('fa-meh-o');
-      me.html('Successfully!');
+    } else {
 
-      //.fa-frown-o
+        smileIcon
+            .addClass('fa-smile-o')
+            .removeClass('fa-meh-o');
 
+    }
 
-      //$('.get-in-touch .form-item').css({opacity: .3});
-  }, 5000);
+}
 
-  //$.post('https://ancient-escarpment-86494.herokuapp.com/', )
+$('#get-in-touch__form').validate({
+    debug: true,
+    errorClass: 'error',
+    errorPlacement: function(error, element) {
+
+        element.before(error);
+
+    },
+    submitHandler: function(form) {
+
+        smile(null);
+
+        submitButton.html(spinnerHTML);
+
+        $.post(
+            'https://e200-mailer.herokuapp.com/',
+            $(form).serialize()
+        ).done(function() {
+
+            $(form)
+                .find(':input')
+                .prop('disabled', 'disabled');
+
+            smile();
+        
+            submitButton.addClass('success');
+            submitButton.text('Successfully!');
+
+        }).fail(function() {
+
+            smile(false);
+
+            submitButton.text('Error! Try again');
+
+        });
+
+    },
+    invalidHandler: function(form, validator) {
+
+        var errors = validator.numberOfInvalids();
+
+        if (errors) {
+
+            smile(false);
+
+            validator.errorList[0].element.focus();
+
+        }
+
+    }
 });
