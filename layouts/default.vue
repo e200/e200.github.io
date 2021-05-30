a1c5791a930745296b9d36125b74a9574cc3c7d0
Eleandro Duzentos <eleandro@inbox.ru>
2021-05-30 23:49:15 +0100

    feat: added navigation component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,17 @@
 <template>
   <main class="main">
+    <Navigation />
+
     <Nuxt />
   </main>
 </template>
+
+<script>
+import Navigation from "../components/navigation";
+
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
