ba2e42e8725134e9c7ab5c485c50cc6e85a18b72
Eleandro Duzentos <eleandro@inbox.ru>
2021-05-30 23:49:00 +0100

    feat: added navigation styles

---
+++ components/navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navigation">
+    <a href="/" class="navigation-brand" :class="classes">Eleandro Duzentos</a>
+
+    <button class="navigation-button" :class="classes" @click="toggleActive">
+      <div class="navigation-button-lines">
+        <span class="navigation-button-line line-0"></span>
+        <span class="navigation-button-line line-1"></span>
+        <span class="navigation-button-line line-2"></span>
+      </div>
+    </button>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  computed: {
+    classes() {
+      return {
+        "is-active": this.isActive,
+      };
+    },
+  },
+};
+</script>
