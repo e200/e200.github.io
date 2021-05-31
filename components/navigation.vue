1fd1123eb3ad42064a556fad7214d43c7754e362
Eleandro Duzentos <eleandro@inbox.ru>
2021-05-31 01:17:52 +0100

    feat: added menu animation

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,20 +1,32 @@
 <template>
   <nav class="navigation">
-    <a href="/" class="navigation-brand" :class="classes">Eleandro Duzentos</a>
+    <div class="container">
+      <a href="/" class="navigation-brand" :class="{ 'is-active': isActive }"
+        >Eleandro Duzentos</a
+      >
 
-    <button class="navigation-button" :class="classes" @click="toggleActive">
-      <div class="navigation-button-lines">
-        <span class="navigation-button-line line-0"></span>
-        <span class="navigation-button-line line-1"></span>
-        <span class="navigation-button-line line-2"></span>
-      </div>
-    </button>
+      <ul class="navigation-list" :class="{ 'is-active': isActive }">
+        <li class="navigation-list-item">Articles</li>
+        <li class="navigation-list-item">Works</li>
+        <li class="navigation-list-item">About</li>
+      </ul>
 
-    <ul class="navigation-list" :class="classes">
-      <li class="navigation-list-item">Articles</li>
-      <li class="navigation-list-item">Works</li>
-      <li class="navigation-list-item">About</li>
-    </ul>
+      <button
+        class="navigation-button"
+        :class="{ 'is-active': isActive }"
+        @click="toggleActive"
+      >
+        <div
+          class="navigation-button-background"
+          :style="backgroundStyles"
+        ></div>
+        <div class="navigation-button-lines">
+          <span class="navigation-button-line line-0"></span>
+          <span class="navigation-button-line line-1"></span>
+          <span class="navigation-button-line line-2"></span>
+        </div>
+      </button>
+    </div>
   </nav>
 </template>
 
@@ -23,22 +35,49 @@ export default {
   data() {
     return {
       isActive: false,
+      hypotenuse: undefined,
+      tapDx: undefined,
+      tapDy: undefined,
     };
   },
   computed: {
-    classes() {
-      return {
-        "is-active": this.isActive,
-      };
+    backgroundStyles() {
+      return [
+        this.isActive
+          ? {
+              width: `${this.hypotenuse * 2}px`,
+              height: `${this.hypotenuse * 2}px`,
+            }
+          : undefined,
+      ];
     },
   },
   methods: {
     toggleActive(e) {
-      const { clientX, clientY } = e
-
-      console.log(clientX, clientY);
-
       this.isActive = !this.isActive;
+
+      if (this.isActive) {
+        this.resolve(e);
+      }
+    },
+    resolve(e) {
+      const { clientX, clientY } = e;
+
+      this.tapDx = clientX;
+      this.tapDy = clientY;
+
+      const centerLeftDistance = clientX;
+      const centerRightDistance = window.innerWidth - clientX;
+
+      const centerTopDistance = clientY;
+      const centerBottomDistance = window.innerHeight - clientY;
+
+      const dx = Math.max(centerLeftDistance, centerRightDistance);
+      const dy = Math.max(centerTopDistance, centerBottomDistance);
+
+      this.hypotenuse = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+
+      console.log(dx, dy, this.hypotenuse);
     },
   },
 };
