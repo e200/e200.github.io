bc83c7cd1ce8f6a7b36e557ffd3a5745b18417f9
Eleandro Duzentos <eleandro@inbox.ru>
2021-06-10 14:23:29 +0100

    feat: added pageCanScroll and resizeCanvas methods

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
-  <main class="main">
-    <Navigation />
+  <main class="main" :class="classes">
+    <canvas id="background-canvas" />
+
+    <Navigation @active="pageCanScroll" />
 
     <Nuxt />
   </main>
@@ -12,6 +14,35 @@ import Navigation from "../components/navigation";
 export default {
   components: {
     Navigation,
+  },
+  computed: {
+    classes() {
+      return {
+        "no-scroll": !this.$store.state.page.isScrollable,
+      };
+    },
+  },
+  methods: {
+    pageCanScroll(value) {
+      this.$store.commit('page/canScroll', !value);
+    },
+    resizeCanvas() {
+      const canvas = document.getElementById('background-canvas');
+
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      console.log(canvas.width);
+    },
+  },
+  mounted() {
+    const context = this
+
+    context.resizeCanvas()
+
+    window.onresize = function() {
+      context.resizeCanvas()
+    }
   },
 };
 </script>
